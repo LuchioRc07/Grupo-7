358744831c3e355eb26924b643630658c859c633
Lucio <102775867+LuchioRc07@users.noreply.github.com>
2022-05-13 13:27:27 -0300

    express

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-let Game
+const express = require('express');
+const app = express();
+
+app.use('/static', express.static(__dirname + '/public'));
+
+app.listen(3100, ()=>{
+    console.log('Servidor operativo');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
