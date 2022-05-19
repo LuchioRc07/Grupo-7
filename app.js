97958267eb01625bb1920908366c503d4394a546
Lucio <102775867+LuchioRc07@users.noreply.github.com>
2022-05-19 19:31:11 -0300

    Express OK

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+//app.use('/static', express.static(__dirname + '/public'));
+
+app.listen(3010, ()=>{
+    console.log('Servidor Grupal corriendo');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
