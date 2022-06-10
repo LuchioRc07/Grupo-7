bf445be830797e4f8db56f14cd717f30cab64cdc
FrancoRugg <francomarceloruggiero@gmail.com>
2022-06-09 22:41:18 -0300

    rutasYejsPorFranco

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require("path");
+
+
 app.use(express.static('public'));
 
+app.set("view engine", "ejs");
+
+const routersMain = require("./routes/main");
+
+app.use("/", routersMain);
 
 app.listen(3010, ()=>{
     console.log('Servidor Grupal corriendo');
 });
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html')});
 
-
-app.get('/login', (req,res)=>{
-        res.sendFile(__dirname + '/views/login.html');
-    });
-    
-app.get('/register', (req,res)=>{
-        res.sendFile(__dirname + '/views/register.html');
-    });
-        
-app.get('/productCart', (req,res)=>{
-        res.sendFile(__dirname + '/views/productCart.html');
-    });
-
-app.get('/productDetail', (req,res)=>{
-        res.sendFile(__dirname + '/views/productDetail.html');
-    });     
-    app.get('/home', (req,res)=>{
-        res.sendFile(__dirname + '/views/home.html');
-    });  
---
+++ routes/main.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+const ejs = require("ejs")
+
+const mainController = require("../controllers/mainController");
+
+router.get("/", mainController.home);
+router.get("/login", mainController.login);
+router.get("/register", mainController.register);
+router.get("/detail", mainController.detail);
+router.get("/cart", mainController.cart);
+
+module.exports = router; 
