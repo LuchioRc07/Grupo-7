2058fadc74c8d9fd716bfb2b72b95e7035a72162
FrancoRugg <francomarceloruggiero@gmail.com>
2022-08-31 19:08:07 -0300

    ? controlle?r

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -117,4 +117,77 @@ const controller = {
 module.exports = controller;
 
 
+// posible controller
 
+// //const { products } = require('../data/productsDB');
+// const db = require('../database/models');
+
+
+// module.exports = {
+//     producto: (req, res) => {
+//         db.Product.findAll({
+//             include: [{association: "category"}, {association: "images"}]
+//         })
+//         .then((products) => {
+//             res.render('Productos', {products, n:false,title:"Productos", session: req.session ? req.session : ""});
+//         })
+//         .catch(err => console.log(err))
+//     },
+//     detalle: (req, res) => {                             //  Fabio
+//         const product = db.Product.findByPk(req.params.idProducto, {
+//             include: [{association: "category"}, {association: "images"}]
+//         })
+//         const slider = db.Product.findAll({
+//             include: [{association: "category"}, {association: "images"}]
+//         })
+//         Promise.all([product,slider])
+//         .then(([product, slider]) => {
+//             if (product !== null){ 
+//                 let sliderProducts = slider.filter(element => element.discount > 0);    //  <----   Sololamente hay 2 productos por categoría, así que el slider muestra los 12 productos que hay hasta el momento
+//                 let texto = product.description.split('\r\n')
+//                 res.render('ProductoDetalle', {
+//                     sliderTitle : "También te pueden interesar",
+//                     sliderProducts,
+//                     product,
+//                     texto,
+//                     n: false,
+//                     title: product.name + " | FTS-Tuning",
+//                     session: req.session ? req.session : ""
+//                 }) 
+//             }else{
+//                 res.send('No tenemos ese producto, pero tenemos muchos otros más...')
+//             }
+//         })
+//         .catch(error => console.log(error))
+//     },
+//     carrito: (req, res) =>{
+//         res.render('Carrito', {title: "Carrito", session: req.session ? req.session : ""})
+//     },
+//     categoria: (req,res) =>{
+//         const category = db.Category.findOne({
+//             where: {
+//                 name: req.params.categoria.trim()
+//             }
+//         })
+//         const products = db.Product.findAll({
+//             include: [{association: "category"}, {association: "images"}]
+//         })
+//         Promise.all([category, products])
+//         .then(([category, products] )=> {
+            
+//             let filtradosPorCategoria = []
+//             products.forEach(producto =>{
+//                 if(producto.category_id === category.id){
+//                     filtradosPorCategoria.push(producto)
+//                 }
+//             })
+            
+            
+//             if(filtradosPorCategoria.length > 0){
+//                 res.render('Productos', {products: filtradosPorCategoria,n: true,category,title:"Categoría", session: req.session ? req.session : ""})
+//             }else {  
+//                 res.render('Productos', {products, title:"Categoria",n: false,category,session: req.session ? req.session : ""})
+//             }   
+//         })
+//     }
+// }
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -213,3 +213,168 @@ module.exports = controller;
 
 
 // module.exports = userController;
+
+
+// controller option
+// const {  validationResult}= require("express-validator")
+// const bcrypt = require('bcryptjs')
+// const session = require("express-session")
+// const db = require('../database/models')
+
+
+// module.exports = {
+//     historial: (req, res) => {
+//         res.render('users/historialCompras', {
+//             title: "Historial", 
+//             session: req.session ? req.session : ""})
+//     },
+//     login: (req, res) => {
+//         res.render('users/Login', {title: "Login", session: req.session ? req.session : ""})
+//     },
+//     register: (req,res) =>{
+//         res.render('users/registro', {title: "registro", session: req.session ? req.session : ""})
+//     },
+    
+//     processLogin: (req, res) => {
+//         let errors = validationResult(req)
+        
+//         if(errors.isEmpty()){
+            
+//             db.User.findOne({
+//                 where:{
+//                     email: req.body.email
+//                 }
+//             })
+//             .then(user=>{
+//                 req.session.user = {
+//                     id: user.id,
+//                     userName: user.firstName + " " + user.lastName,
+//                     rol: user.rol
+//                 }
+//                 /** creamos la cookie */
+//                 if(req.body.remember){
+//                    res.cookie('cookieFTS', req.session.user, {maxAge: 1000*60})
+//                 }
+//                 /------------------/
+//                 /** guardamos el usuario en locals */
+//                 res.locals.user = req.session.user
+//                 /**redireccionamos al home si todo esta ok */
+//                 res.redirect('/')
+//             })
+//             .catch(err=> console.log(err))
+         
+//          /**sino -> */
+//         }else{
+//             res.render('users/Login', {
+//                 title: "Login",
+//                 errors: errors.mapped(),
+//                 session: req.session ? req.session : ""
+//             })
+//         }
+
+//     },   
+//     proccesRegister: (req,res) => {
+//         let errors = validationResult(req)
+//         if(errors.isEmpty()){
+//             let{
+//                 name,
+//                 lastName, 
+//                 email,
+//                 password1
+//             } = req.body
+    
+//             db.User.create(
+//                 {
+//                     firstName: name,
+//                     lastName,
+//                     email, 
+//                     password: bcrypt.hashSync(password1, 10),
+//                     rol: 'user',
+//                     image: "default-image.png",
+//                     province: '',
+//                     address: '',
+//                     cp: 0,
+//                     city: ''
+//                 }
+//             )
+//                 .then(()=>{
+//                     res.redirect('/users/login')
+//                 })
+//                 .catch(err=> console.log(err))
+
+//         }else{
+//             res.render('users/registro', {
+//                 title: "Registro",
+//                 errors :errors.mapped(),
+//                 old : req.body,
+//                 session: req.session ? req.session : ""
+//             })
+//         }
+//     },
+//     accountEdit: (req, res) => {
+//         db.User.findOne({
+//             where: {
+//                 id: req.session.user.id
+//             }
+//         })
+//         .then(user => {
+//             res.render('users/accountEdit', {title: "Edita tu cuenta", session: req.session, user})
+//         })
+//     },
+
+
+
+//     perfilUsers: (req, res) => {
+//         db.User.findOne({
+//             where: {
+//                 id: req.session.user.id
+//             }
+//         })
+//         .then(user => {
+//             res.render('users/perfil', {title: "perfil de usuario", session: req.session, user})
+//         })
+//     },
+//     userEdit: (req,res) =>{
+//         let errors = validationResult(req)
+//         if(errors.isEmpty()){
+//             db.User.update({
+//                 firstName: req.body.firstName,
+//                 lastName: req.body.lastName,
+//                 address: req.body.address,
+//                 cp: req.body.cp,
+//                 province: req.body.provincia,
+//                 city: req.body.localidad,
+//                 image: req.file && req.file.filename
+//             },{
+//                 where:{
+//                     id: +req.session.user.id
+//                 }
+//             })
+//             .then(() => {
+//                 req.session.user = {
+//                     id: req.session.user.id,
+//                     userName: req.body.firstName + " " + req.body.lastName,
+//                     rol: req.session.user.rol
+//                 }
+//             res.redirect('/') 
+//             })
+//             .catch(err=> console.log(err))
+//         }else{
+//             db.User.findOne({
+//                 where: {
+//                     id: req.session.user.id
+//                 }
+//             })
+//             .then(user => {
+//                 res.render('users/accountEdit', {title: "Edita tu cuenta", session: req.session, user,errors :errors.mapped() ,old : req.body})
+//             })
+//         }
+//     },
+//     logout: (req, res) => {
+//         req.session.destroy();
+//         if(req.cookies.cookieFTS){
+//             res.cookie('cookieFTS','',{maxAge:-1})
+//         }
+//         res.redirect('/')
+//     }
+// }
