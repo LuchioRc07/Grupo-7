4eb6af670e15477a2d243ff8f7f4b63bd84a6f83
Gianfranco Policari <103081719+GianPolicari@users.noreply.github.com>
2022-09-22 21:01:57 -0300

    fc

--- public/js/validaciones.js
+++ public/js/validaciones.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function(){
     var formulario = document.querySelector('form.form');
 
     formulario.addEventListener('submit', function(e) {
-        e.preventDefault();
+        
 
         var campoNombre = document.querySelector('input.log');
 
