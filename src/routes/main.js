96f98afff8bd16db666bfbcdf24ed3d8e37cbd65
FrancoRugg <francomarceloruggiero@gmail.com>
2022-09-27 20:16:41 -0300

    validations of products, login and register

--- src/routes/main.js
+++ src/routes/main.js
@@ -6,8 +6,8 @@ const ejs       = require("ejs")
 const mainController = require("../controllers/mainController");
 
 router.get("/", mainController.home);
-router.get("/login", mainController.login);
-router.get("/register", mainController.register);
-router.get("/cart", mainController.cart);
+// router.get("/login", mainController.login);
+// router.get("/register", mainController.register);
+// router.get("/cart", mainController.cart);
 
 module.exports = router; 
